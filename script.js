44f5edc12ac591db716be56a888173bc0e17b533
bharath02233 <bharath02233@gmail.com>
2025-02-21 15:41:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
-  // your code here
+    if (!str) return [];
+
+    let result = [];
+    for (let i = 0; i < str.length; i += chunkLength) {
+        result.push(str.substring(i, i + chunkLength));
+    }
+    return result;
 }
 
 // Do not change the code below
